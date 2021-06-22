0be78696973a90581e28211721d01a7be4dc8150
aitzaz-ahmad <cptn.aitzaz@gmail.com>
2021-06-23 01:15:16 +0200

    adds the solution of the reformat-the-string problem

---
+++ reformat-the-string/solution.cpp
@@ -0,0 +1,47 @@
+string reformat(string s) {
+
+        if(s.size() < 2)
+            return s;
+
+        auto digits = 0;
+        auto alphabets = 0;
+
+        for(auto ch: s)
+            if(std::isdigit(ch))
+                ++digits;
+            else
+                ++alphabets;
+
+        auto isEven = s.size() % 2 == 0;
+        if((isEven && digits != alphabets) ||
+          (!isEven && std::abs(digits - alphabets) != 1))
+            return "";
+
+        constexpr std::string_view DIGITS = "1234567890";
+        auto prevDigit = s.find_first_of(DIGITS);
+        auto prevAlpha = s.find_first_not_of(DIGITS);
+        auto reformatted = string {};
+
+        while(reformatted.size() < s.size())
+        {
+            if(digits > alphabets)
+            {
+                reformatted += s[prevDigit];
+
+                if(prevAlpha != string::npos)
+                    reformatted += s[prevAlpha];
+            }
+            else
+            {
+                reformatted += s[prevAlpha];
+
+                if(prevDigit != string::npos)
+                    reformatted += s[prevDigit];
+            }
+
+            prevDigit = s.find_first_of(DIGITS, ++prevDigit);
+            prevAlpha = s.find_first_not_of(DIGITS, ++prevAlpha);
+        }
+
+        return reformatted;
+    }
